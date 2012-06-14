1354afd56066507bffe6917639c264a68c83bac8
Jeffrey Warren <jeff@unterbahn.com>
2012-06-14 00:16:30 -0400

    initial commit

---
+++ extract_filesizes_spark.rb
@@ -0,0 +1,10 @@
+# first run "ls -l > sizes.txt" in the directory of images
+# then run this script, dumping the output into a file: "ruby extract_filesizes.rb > sizestring.txt"
+# then copy it into the index.html file to be graphed
+output = []
+input = File.open("sizes.txt","r").read.split("\n")
+input.slice!(0,1)
+input.each_with_index do |line,i|
+	output << line.split(' ')[4]
+end
+puts output.join(' ')
---
+++ extract_temperatures.rb
@@ -0,0 +1,7 @@
+# exiftool * | grep Temperature > ../filesizes/vis-temp.txt
+output = []
+input = File.open("ir-temp.txt","r").read.split("\n")
+input.each_with_index do |line,i|
+	output << "["+i.to_s+","+line.split(': ')[1].split(' ')[0]+"]"
+end
+puts output.join(',')
